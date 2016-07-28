33ee7616a065880e160ec25cbdc70d722b3b589a
earshel <me@earshel.com>
2016-07-28 03:24:48 -0700

    Update snipe.bat

--- pogo/snipe.bat
+++ pogo/snipe.bat
@@ -1,1 +1,1 @@
-snipe.py -a "google or ptc" -u "username" -p "password" -l "starting loc format 123.002334,11.3838484"
+snipe.py -a "google or ptc" -u "username" -p "password" -l "starting loc format 123.002334 11.3838484"
